96805912ba39396639a7dfae9d2ed548f7e2eef1
toshi09 <toshi09vs@gmail.com>
2016-04-28 11:27:29 -0700

    Update 2011_ED_FORWARD.py

--- 2011_ED_FORWARD.py
+++ 2011_ED_FORWARD.py
@@ -1,3 +1,15 @@
+'''Get the visit distribution and number of patients who were admitted to the emergency department at least once in 2011
+and who were hpspitalized at least once more(Emergency department or otherwise) in 2012 '''
+
+'''Get the visit distribution and number of patients who were admitted to the emergency department at least once in 2011
+and who were hpspitalized at least once more(Emergency department or otherwise) in 2013 '''
+
+'''Get the visit distribution and number of patients who were admitted to the emergency department at least once in 2011
+and who were hpspitalized at least once more(Emergency department or otherwise) in 2012 or 2013 '''
+
+
+
+
 from column_index_map import *
 from collections import  defaultdict
 
@@ -24,17 +36,38 @@ def visit_freq(visits):
         dict_obj[element] = dict_obj[element] + 1
     return dict_obj
 
-def rem(inputfile,outfile):
+def get_visit_distribution_against_against_ed_base_year(inputfile, base_year, other_years):
+    """
+    Get patients with ED visits in base year.
+    For all such patients find the visit distribution in other_years.
+    Other year is list of years, e.g ['2012'], ['2013'] or ['2012', '2013']
+    This function prints the ED visit distribution in the base year.
+    It also prints the visit distribution of all such patiens in the other years.
+    Visit distribution is cumulative visit distribution in three categories:
+    4 visits, 8 visits, 16 visits.
+    :param inputfile: patient record file
+    :param base_year: base year for patients with ED (Emeregence Department) visits
+    :param other_years: Other years for which we want to find the visit distribution
+    for all the patients sastisfying the criterion in base year.
+    :return: prints number of patients in base year and other years. Also prints
+    the visit distribution in base year and other year.
+    """
+    """
+    :param inputfile:
+    :param base_year:
+    :param other_years:
+    :return:
+    """
     filehandle = open(inputfile)
     garbage = filehandle.readline()
 
     patient_ids = set()
-    patient_ids_2012 = set()
-    patient_ids_2013 = set()
+    patient_ids_from_other_years = set()
+
     for line in filehandle:
         values = line.strip().split(",")
 
-        if values[SRC_COL_IDX] == '1' and values[ADMIT_YEAR_IDX] == '2011':
+        if values[SRC_COL_IDX] == '1' and values[ADMIT_YEAR_IDX] == base_year:
             patient = values[32]
             patient_ids.add(patient)
 
@@ -45,11 +78,10 @@ def rem(inputfile,outfile):
     prev_id = ''
     visit = []
     filehandle = open(inputfile)
-    w_fhand = open(outfile,'w')
-    visit_dist_2011 = defaultdict(int)
-    visit_dist_2012 = defaultdict(int)
-    visit_dist_2013 = defaultdict(int)
 
+    visit_dist_base_year = defaultdict(int)
+    visit_dist_for_other_years = defaultdict(int)
+    other_years_str = " ".join(other_years)
     for line in filehandle:
         values = line.strip().split(",")
         patient = values[32]
@@ -58,29 +90,33 @@ def rem(inputfile,outfile):
 
         if prev_id != patient:
             per_year_visit_cnt = visit_freq(visit)
-            update_visit_distibution(visit_dist_2011, per_year_visit_cnt['2011'])
-            update_visit_distibution(visit_dist_2012, per_year_visit_cnt['2012'])
-            update_visit_distibution(visit_dist_2013, per_year_visit_cnt['2013'])
+            update_visit_distibution(visit_dist_base_year, per_year_visit_cnt[base_year])
+            update_visit_distibution(visit_dist_for_other_years, per_year_visit_cnt[other_years_str])
+
             visit = []
 
-        if values[SRC_COL_IDX] == '1' and values[ADMIT_YEAR_IDX] == '2011':
-            visit.append('2011')
-            w_fhand.write(line)
+        if values[SRC_COL_IDX] == '1' and values[ADMIT_YEAR_IDX] == base_year:
+            visit.append(base_year)
 
-        elif values[ADMIT_YEAR_IDX] == '2012':
-            visit.append('2012')
-            w_fhand.write(line)
-            patient_ids_2012.add(patient)
 
-        elif values[ADMIT_YEAR_IDX] == '2013':
-            visit.append('2013')
-            w_fhand.write(line)
-            patient_ids_2013.add(patient)
+        elif values[ADMIT_YEAR_IDX] in other_years:
+            visit.append(other_years_str)
+            patient_ids_from_other_years.add(patient)
+
         prev_id = patient
-
     filehandle.close()
-    w_fhand.close()
-    print len(patient_ids), len(patient_ids_2012), len(patient_ids_2013)
-    print visit_dist_2011,  visit_dist_2012, visit_dist_2011
 
-rem('/Users/oshpddata/Desktop/OSHPD2016/OSHPD_ALLCAUSE.csv','/Users/oshpddata/Desktop/vikhyati/2011_ED_FORWARD.csv' )
+    print len(patient_ids), len(patient_ids_from_other_years)
+    print visit_dist_base_year,  visit_dist_for_other_years
+
+'''Get the visit distribution and number of patients who were admitted to the emergency department at least once in 2011
+and who were hpspitalized at least once more(Emergency department or otherwise) in 2012 '''
+get_visit_distribution_against_against_ed_base_year('/Users/oshpddata/Desktop/OSHPD2016/OSHPD_ALLCAUSE.csv','2011', ['2012'])
+
+'''Get the visit distribution and number of patients who were admitted to the emergency department at least once in 2011
+and who were hpspitalized at least once more(Emergency department or otherwise) in 2013 '''
+get_visit_distribution_against_against_ed_base_year('/Users/oshpddata/Desktop/OSHPD2016/OSHPD_ALLCAUSE.csv', '2011', ['2013'])
+
+'''Get the visit distribution and number of patients who were admitted to the emergency department at least once in 2011
+and who were hpspitalized at least once more(Emergency department or otherwise) in 2012 or 2013 '''
+get_visit_distribution_against_against_ed_base_year('/Users/oshpddata/Desktop/OSHPD2016/OSHPD_ALLCAUSE.csv', '2011', ['2012', '2013'])
